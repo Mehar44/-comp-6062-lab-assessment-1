3839e6ba576f3d1a64d88de42f76c2f0b621ddd7
Mehar44 <149817172+Mehar44@users.noreply.github.com>
2024-06-11 19:31:32 -0400

    Update app.js

--- css/scripts/app.js
+++ css/scripts/app.js
@@ -1,7 +1,7 @@
-
 const studentName = 'Mehardeep Singh';
 const studentNumber = '1150979';
-console.log(studentName + ' - ' + studentNumber);
-const headerContent = document.querySelector('h1');
-headerContent.innerHTML = studentName + ' - ' + studentNumber;
-headerContent.classList.add('heading1');
+const result = (studentName).concat('-', studentNumber);
+console.log(result);
+const headrContent=document.querySelector("h1");
+headrContent.innerHTML=result;
+headrContent.classList.add("headings");
