e1cade24076dbf4b41531dbd9daa9acfa117187c
Mehar44 <149817172+Mehar44@users.noreply.github.com>
2024-06-11 19:27:45 -0400

    lab

---
+++ css/scripts/app.js
@@ -0,0 +1,7 @@
+
+const studentName = 'Mehardeep Singh';
+const studentNumber = '1150979';
+console.log(studentName + ' - ' + studentNumber);
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = studentName + ' - ' + studentNumber;
+headerContent.classList.add('heading1');
